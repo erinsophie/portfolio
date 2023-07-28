e13b9775c499052b1c86fb8ecf5bbf88c548d367
Sophie Little <119007056+erinsophie@users.noreply.github.com>
2023-07-28 13:38:46 +0100

    Extract project info into file

---
+++ src/components/ProjectData.js
@@ -0,0 +1,14 @@
+import weatherAppImg from '../assets/img/weather-app.png';
+
+const projectsList = [
+  {
+    title: 'Weather App',
+    description: `Search for any city and the current data, including local time, temperature and condition will be displayed.`,
+    site: 'https://erinsophie.github.io/weather-app/',
+    source: 'https://github.com/erinsophie/weather-app',
+    image: weatherAppImg,
+    tech: 'HTML, CSS, Javascript, Weather API',
+  },
+];
+
+export default projectsList;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,18 +1,8 @@
 import '../styles/Projects.css';
 import Project from './Project';
-import weatherAppImg from '../assets/img/weather-app.png';
+import projectsList from './ProjectData'
 
 function Projects() {
-  const projectsList = [
-    {
-      title: 'Weather App',
-      description: `Search for any city and the current data, including local time, temperature and condition will be displayed. Toggle to see either today's forecast or this week's forecast. Displays the next 12 hours of weather, adjusting for the city's local time.`,
-      site: 'https://erinsophie.github.io/weather-app/',
-      source: 'https://github.com/erinsophie/weather-app',
-      image: weatherAppImg,
-      tech: 'HTML, CSS, Javascript, Weather API',
-    },
-  ];
 
   return (
     <div className="projects">
